b06231506fb8da80a400c11dbda47148f3f83ab5
20221tiimi0300 <marcellagaurink@gmail.com>
2024-06-18 09:04:08 -0300

    continuacao atividade dois

--- app/src/main/java/gaurink/marcella/galeria/MainAdapter.java
+++ app/src/main/java/gaurink/marcella/galeria/MainAdapter.java
@@ -1,6 +1,10 @@
 package gaurink.marcella.galeria;
 
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,16 +23,29 @@ public class MainAdapter extends RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(mainActivity);
+        View v = inflater.inflate(R.layout.list_item,parent,false);
+        return new MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        ImageView imPhoto = holder.itemView.findViewById(R.id.imItem);
+        int w = (int) mainActivity.getResources().getDimension(R.dimen.itemWidth);
+        int h = (int) mainActivity.getResources().getDimension(R.dimen.itemHeight);
+        Bitmap bitmap = Util.getBitmap(photos.get(position), w, h);
+        imPhoto.setImageBitmap(bitmap);
+        imPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v) {
+                mainActivity.startPhotoActivity(photos.get(position));
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return photos.size();
     }
 }
